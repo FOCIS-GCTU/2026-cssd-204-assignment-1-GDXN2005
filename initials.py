14f0b90b95840e64084d1de483fd4681bd7bc8c4
GDXN2005 <gfiadzigbey@gmail.com>
2026-05-31 09:12:31 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,42 @@
+# File: initials.py
+# Description: Print out my initials in large block letters.
+# Assignment Number: 1
+#
+# Name: Godson Buernortey Fiadzigbey
+# STUDENT ID: 2425402121
+# Email: gfiadzigbey@gnail.com
+# Grader: Augustus buckman
+
 
 def main():
-  pass #code goes here
+    # This function prints stylized large initials with proper padding and block periods.
 
+    print()
+    print("...GBF")
+    print()
+    
+    # Line 1
+    print("...GGGGGGGGGGGG...BBBBBBBBBBB....FFFFFFFFFFFF...")
+    # Line 2
+    print("...GGGGGGGGGGGG...BBBBBBBBBBBB...FFFFFFFFFFFF...")
+    # Line 3
+    print("...GGGG...........BBBB....BBBB...FFFF...........")
+    # Line 4
+    print("...GGGG...........BBBB....BBBB...FFFF...........")
+    # Line 5
+    print("...GGGG...GGGGG...BBBBBBBBBBB....FFFFFFFFF......")
+    # Line 6
+    print("...GGGG...GGGGG...BBBBBBBBBBB....FFFFFFFFF......")
+    # Line 7
+    print("...GGGG....GGGG...BBBB....BBBB...FFFF...........")
+    # Line 8
+    print("...GGGG....GGGG...BBBB....BBBB...FFFF...........")
+    # Line 9
+    print("...GGGGGGGGGGGG**...BBBBBBBBBBBB**...FFFF........**...")
+    # Line 10
+    print("...GGGGGGGGGGGG**...BBBBBBBBBBB.**...FFFF........**...")
+    
+    print()
 
 
 main()
